45640a08d6b9f57f8298f20d8d4a14ba2ed3d8b6
mah-qurashy <diz49@live.com>
2020-12-13 22:16:36 +0200

    add book modal

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,3 +1,4 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Place } from 'src/app/places/place.model';
@@ -8,16 +9,42 @@ import { Place } from 'src/app/places/place.model';
   styleUrls: ['./create-booking.component.scss'],
 })
 export class CreateBookingComponent implements OnInit {
-  @Input() selectedPlace: Place
-  
+  @Input() place: Place
+  private currentCostText="Pick date to calculate cost"
+  private startDate: string
+  private endDate: string
+  private todayDate=new Date(Date.now()).toISOString()
   constructor(private modalController: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.todayDate)
+  }
+  onStartDateChange(event: CustomEvent){
+    this.startDate=event.detail.value;
+
+    if(this.endDate){
+      this.caclulateCost(this.startDate,this.endDate)
+    }
+  }
+  onEndDateChange(event: CustomEvent){
+    this.endDate=event.detail.value;
+    if(this.startDate){
+
+      this.caclulateCost(this.startDate,this.endDate)
+    }
+  }
   onBookPlace(){
     this.modalController.dismiss(null,'cancel')
   }
   onCancel(){
     this.modalController.dismiss({message:'booked'},'confirm')
+  }
+  caclulateCost(ds1:string,ds2:string){
+    let d1=new Date(ds1)
+    let d2=new Date(ds2)
+    let diff = Math.ceil(Math.abs(d2.getTime()-d1.getTime()) / (1000 * 60 * 60 * 24)); 
+    this.currentCostText="Total Cost = "+diff*this.place.price
+
   }
 
 }
