d7ca2474313c9bb323af62267dd6774cc9807333
mah-qurashy <diz49@live.com>
2020-12-14 01:02:13 +0200

    add form to new offer page

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
-
+  private startDate: string
+  private endDate: string
+  private todayDate=new Date(Date.now()).toISOString()
   constructor() { }
 
   ngOnInit() {
   }
+  onStartDateChange(event: CustomEvent){
+    this.startDate=event.detail.value;
+  }
+  onEndDateChange(event: CustomEvent){
+    this.endDate=event.detail.value;
+
+  }
+  onSubmit(form: NgForm){
+    const title=form.value.title
+    const description=form.value.description
+    const startDate=form.value.startdate
+    const endDate=form.value.enddate
+    const price=form.value.price
+  }
+
 
 }
