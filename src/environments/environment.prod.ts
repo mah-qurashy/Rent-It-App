8e4edd9e340c51a2de102a801f9058416994cc12
mah-qurashy <diz49@live.com>
2020-12-15 04:19:54 +0200

    add firebase to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBAF0IUWXTCnjcac9yC_07n9tMyXS8zAn0",
+    authDomain: "rent-it-35be1.firebaseapp.com",
+    databaseURL: "https://rent-it-35be1-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "rent-it-35be1",
+    storageBucket: "rent-it-35be1.appspot.com",
+    messagingSenderId: "777013822634",
+    appId: "1:777013822634:web:c22bec0cf63a49655cbcd3",
+    measurementId: "G-SYRZG4Z403"
+  }
 };
